c140a65817b1e73c3b123a0ba12aa7b5d57e1631
jibuapprepo <jibu@napvibe.com>
2025-08-19 20:57:53 +0300

    Update jibu.page.ts

--- src/core/features/jibu/jibu.page.ts
+++ src/core/features/jibu/jibu.page.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
