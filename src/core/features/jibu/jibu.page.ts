8d6285560d3c2fd6b29245f31257eb8f2e510e3a
jibuapprepo <jibu@napvibe.com>
2025-08-19 20:16:04 +0300

    Update jibu.page.ts

--- src/core/features/jibu/jibu.page.ts
+++ src/core/features/jibu/jibu.page.ts
@@ -1,1 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
+@Component({
+  selector: 'app-jibu',
+  templateUrl: './jibu.page.html',
+  styleUrls: ['./jibu.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+})
+export class JibuPage {
+  title = 'Welcome to Jibu App 🚀';
+}
